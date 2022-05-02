b5250b72a78852f3a00f5d1da66d141242008d1b
two-six <twopsix@gmail.com>
2022-05-03 01:29:30 +0200

    formatting fix

--- server/main.js
+++ server/main.js
@@ -165,24 +165,24 @@ function readJson(res, cb, err) {
     res.onData((ab, isLast) => {
         let chunk = Buffer.from(ab);
         if (isLast) {
-        let json;
-        if (buffer) {
-            try {
-            json = JSON.parse(Buffer.concat([buffer, chunk]));
-            } catch (e) {
-            res.close();
-            return;
+            let json;
+            if (buffer) {
+                try {
+                    json = JSON.parse(Buffer.concat([buffer, chunk]));
+                } catch (e) {
+                    res.close();
+                    return;
+                }
+                cb(json);
+            } else {
+                try {
+                    json = JSON.parse(chunk);
+                } catch (e) {
+                    res.close();
+                    return;
+                }
+                cb(json);
             }
-            cb(json);
-        } else {
-            try {
-            json = JSON.parse(chunk);
-            } catch (e) {
-            res.close();
-            return;
-            }
-            cb(json);
-        }
         } else {
             if (buffer) {
                 buffer = Buffer.concat([buffer, chunk]);
