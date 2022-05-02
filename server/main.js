a8972f1433706f02e8fc0c0b457a1ff44a1a9931
two-six <twopsix@gmail.com>
2022-05-02 15:28:17 +0200

    /user/login

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
+import {createHash} from 'crypto';
 import IG from 'instagram-web-api';
-import fetch from 'node-fetch';
+import fetch, { AbortError } from 'node-fetch';
 embed('server/app.js');
 embed('server/static.js');
 
@@ -33,7 +34,7 @@ FS.readdirSync('assets/instagram').forEach(file => {
     app.get(`/photos/${file}`, res => {
         res.onAborted(() => { });
         res.writeHeader('Content-Type', 'image/png');
-        res.end(data);
+        res.end(data)
     });
 });
 
@@ -45,3 +46,57 @@ app.get('/photos', res => {
     res.writeHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(photos));
 });
+
+app.post('/user/login', res => {
+    readJson(res, (obj) => {
+        res.end(jwt(obj));
+    }, () => {
+        res.end()
+    });
+});
+
+function readJson(res, cb, err) {
+  let buffer;
+  res.onData((ab, isLast) => {
+    let chunk = Buffer.from(ab);
+    if (isLast) {
+      let json;
+      if (buffer) {
+        try {
+          json = JSON.parse(Buffer.concat([buffer, chunk]));
+        } catch (e) {
+          res.close();
+          return;
+        }
+        cb(json);
+      } else {
+        try {
+          json = JSON.parse(chunk);
+        } catch (e) {
+          res.close();
+          return;
+        }
+        cb(json);
+      }
+    } else {
+      if (buffer) {
+        buffer = Buffer.concat([buffer, chunk]);
+      } else {
+        buffer = Buffer.concat([chunk]);
+      }
+    }
+  });
+
+  res.onAborted(err);
+}
+
+function jwt(ctx) {
+  const header = Buffer.from(JSON.stringify({
+    "alg": "SHA256",
+    "typ": "JWT"
+  })).toString('base64');
+  const content = Buffer.from(JSON.stringify(ctx)).toString('base64');
+  const sec = Buffer.from(FS.readFileSync("secret.txt")).toString('base64');
+  const hash = createHash('sha256').update(header + Buffer.from(".").toString('base64') + content + Buffer.from(".").toString('base64') + sec).digest('base64');
+  return hash;
+}
